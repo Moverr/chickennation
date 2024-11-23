87594807517df6a4c6536e04fc739dd7502311d7
Muyinda Rogers <muyinda.rodgers@wasoko.com>
2024-11-23 21:44:32 +0300

    Init : some new itesm

---
+++ src/main/java/com/khoodilabs/chickennation/ChickennationApplication.java
@@ -0,0 +1,13 @@
+package com.khoodilabs.chickennation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChickennationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChickennationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khoodilabs/chickennation/ChickennationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khoodilabs.chickennation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChickennationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
